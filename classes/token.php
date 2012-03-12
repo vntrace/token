25e39e0cf301b2684fef7544ad87a62385d31b57
thanhdang <clgt@clgt.vn>
2012-03-12 22:58:26 +0700

    add new resource

---
+++ classes/token.php
@@ -0,0 +1,16 @@
+<?php
+class Token{
+	public static function authen($token){
+		$api_auth_url = Kohana::$config->load('token.api_auth_url');
+
+		$request = Request::factory($api_auth_url);
+
+		$request->headers('Clgt-Auth-Key', $token);
+
+		$response = $request->execute();
+
+		if($response->status(200)) return true;
+
+		return false;
+	}
+}
---
+++ config/token.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+	'api_auth_url' => 'http://api.clgt.vn/api/auth'
+);
