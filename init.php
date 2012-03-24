beafbb027d34990b56ee16b7af3790c5abc1740a
thanhdang <clgt@clgt.vn>
2012-03-25 01:10:11 +0700

    Update

--- init.php
+++ init.php
@@ -2,9 +2,6 @@
 
 // Defined constant will be used
 
-!defined('API_URL') ? define('API_URL', 'http://dev.api') : false;
+!defined('API_URL') ? define('API_URL', 'http://api.clgt.vn') : false;
 
 !defined('API_AUTH_PATH') ? define('API_AUTH_PATH', '/api/auth') : false;
-
-// Default api for all of clgt's apps
-!defined('API_KEY') ? define('API_KEY', '8394af998a9b36ac43ce012f9a9c6dec') : false;
